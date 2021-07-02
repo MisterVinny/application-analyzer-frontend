6ec89a66801ad7af01b10455e53df82251e94ff4
vinnyk <ragdolleye@gmail.com>
2021-07-02 13:48:17 -0500

    Adds formatting to the applications index div boxes.

--- src/views/Applications.vue
+++ src/views/Applications.vue
@@ -4,7 +4,21 @@
       <router-link to="/applications/new">Add Application</router-link>
     </div>
     <div v-for="application in applications" v-bind:key="application.id">
-      <p>{{ application }}</p>
+      <ul class="no-bullets">
+        <li><b>Employer: </b>{{ application.employer }}</li>
+        <li><b>Address: </b>{{ application.address }}</li>
+        <li><b>Position: </b>{{ application.position }}</li>
+        <li><b>Date Applied: </b>{{ application.date }}</li>
+        <li><b>Contact: </b>{{ application.contact }}</li>
+        <li><b>Contact Email: </b>{{ application.email }}</li>
+        <li><b>Followup: </b>{{ application.followup }}</li>
+        <li><b>Interviews: </b>{{ application.interviews }}</li>
+        <li><b>Status: </b>{{ application.status }}</li>
+        <li><b>Method: </b>{{ application.method }}</li>
+        <li><b>Enthusiasm: </b>{{ application.enthusiasm }}</li>
+        <li><b>Confidence: </b>{{ application.confidence }}</li>
+        <li><b>Notes: </b>{{ application.notes }}</li>
+      </ul>
       <router-link :to="`/applications/${application.id}/edit`"
         >Edit</router-link
       >
